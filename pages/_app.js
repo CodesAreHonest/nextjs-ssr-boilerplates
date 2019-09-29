171d86af1a0bd9a6cee5582506110fcdbf9e9e23
Yinghua <chai.yh@worldcloudventures.com>
2019-09-29 22:43:35 +0800

    React Redux setup that breaks automatic prerendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import App from 'next/app';
-import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../styled/theme/default';
+import withReduxStore from "../lib/with-redux-store";
+import { Provider } from 'react-redux';
 
 class MyApp extends App {
     componentDidMount() {
@@ -15,21 +16,18 @@ class MyApp extends App {
     }
 
     render() {
-        const { Component, pageProps } = this.props;
+        const { Component, pageProps, reduxStore } = this.props;
 
         return (
-            <>
-                <Head>
-                    <title>My page</title>
-                </Head>
-                <ThemeProvider theme={theme}>
-                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                    <CssBaseline/>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline/>
+                <Provider store={reduxStore}>
                     <Component {...pageProps} />
-                </ThemeProvider>
-            </>
+                </Provider>
+            </ThemeProvider>
         );
     }
 }
 
-export default MyApp;
+export default withReduxStore(MyApp);
